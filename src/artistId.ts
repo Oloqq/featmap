d5841110b0f0aaf34e27b694e035fd63e4e80ab4
Oloqq <olgierdpk@gmail.com>
2022-04-14 21:52:46 +0200

    artist id extraction

--- src/artistId.ts
+++ src/artistId.ts
@@ -1,5 +1,31 @@
-function fromUri() {
-  let a;
-  
-  return 1;
+let extractId = {
+  fromUri: function (uri: string): string {
+    let tmp =  uri.split(':');
+    if (tmp[0] !== 'spotify' || tmp[1] !== 'artist') {
+      throw new Error('Bad uri');
+    }
+    return tmp[2];
+  },
+
+  fromLink: function (link: string): string {
+    let re = /^http.+artist\/(.+)\?si=.+$/;
+    let matches = link.match(re);
+    if (matches) {
+      return matches[1];
+    }
+    else {
+      throw new Error('Bad link');
+    }
+  },
+
+  fromAny: function (input: string): string {
+    if (input.startsWith('http')) {
+      return this.fromLink(input);
+    }
+    else {
+      return this.fromUri(input);
+    }
+  }
 }
+
+export { extractId };
--- test/userInput.ts
+++ test/userInput.ts
@@ -1,18 +1,29 @@
 // import { Options } from '../src/Classes/Options/Options'; // this will be your custom import
-import '../src/artistId';
+import { extractId } from '../src/artistId';
 import { expect } from 'chai';
 
-describe('Extracting artist id', () => { // the tests container
-    it('from uri', () => { // the single test
-        // const options = new Options(); // this will be your class
+describe('Extracting artist id', () => {
+    let link = 'https://open.spotify.com/artist/0Wi2fADbhwXlPUWxBmzo99?si=85a1719cac0c4afd';
+    let uri = 'spotify:artist:0Wi2fADbhwXlPUWxBmzo99';
 
-        //         /* detect retina */
-        // expect(options.detectRetina).to.be.false; // Do I need to explain anything? It's like writing in English!
-
-        // /* fps limit */
-        // expect(options.fpsLimit).to.equal(30); // As I said 3 lines above
-
-        // expect(options.interactivity.modes.emitters).to.be.empty; // emitters property is an array and for this test must be empty, this syntax works with strings too
-        // expect(options.particles.color).to.be.an("object").to.have.property("value").to.equal("#fff"); // this is a little more complex, but still really clear
+    it('from uri', () => {
+      expect(extractId.fromUri(uri)).to.eq('0Wi2fADbhwXlPUWxBmzo99');
     });
+
+    it('from link', () => {
+      expect(extractId.fromLink(link)).to.eq('0Wi2fADbhwXlPUWxBmzo99');
+    });
+
+    it('from any when passed an uri', () => {
+      expect(extractId.fromAny(uri)).to.eq('0Wi2fADbhwXlPUWxBmzo99');
+    });
+
+    it('from any when passed a link', () => {
+      expect(extractId.fromAny(link)).to.eq('0Wi2fADbhwXlPUWxBmzo99');
+    });
+
+    it('throws on bad uri format ', () => {
+      expect(() => extractId.fromAny('skrrrr:asga:0Wi2fADbhwXlPUWxBmzo99'))
+        .to.throw('Bad uri');
+    })
 });
